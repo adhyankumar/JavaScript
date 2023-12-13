ad586353d3fb683a093069446914a42c8f522139
Adhyan kumar <142704937+adhyankumar@users.noreply.github.com>
2023-12-13 19:49:25 +0530

    Date_time_concept

---
+++ Date_time/index.js
@@ -0,0 +1,24 @@
+// creata a Date
+const d = Date(2002-7-5)
+// console.log(d);
+
+//  three are 9 ways to create date
+new Date()
+const dt = new Date()
+// console.log(dt);
+
+// new Date(date string)
+const dt2 = new Date("December 9, 2023,10:22")
+// console.log(dt2);
+
+// new Date(year,month)
+
+const dt3 = new Date(2023,1)
+console.log(dt3);
+// new Date(year,month,day)
+// new Date(year,month,day,hours)
+// new Date(year,month,day,hours,minutes)
+// new Date(year,month,day,hours,minutes,seconds)
+// new Date(year,month,day,hours,minutes,seconds,ms)
+
+// new Date(milliseconds)
